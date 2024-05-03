f5a3613865d08699a29e09ff08bc3c100da6df54
cyril0124 <211998078@qq.com>
2024-05-03 16:17:53 +0800

    Update setvars.sh

--- setvars.sh
+++ setvars.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-PRJ_DIR=$(dirname $(realpath $0))
+current_shell=$(ps -p $$ -ocomm=)
+
+if [[ "$current_shell" == *bash* ]]; then
+    script_path=$(realpath "${BASH_SOURCE[0]}")
+elif [[ "$current_shell" == *zsh* ]]; then
+    script_path=$0
+else
+    echo "Unknown shell"
+fi
+
+PRJ_DIR=$(dirname $(realpath $script_path))
 
 export LUA_PATH="\
 $LUA_PATH\
